6296faf003c58a2b1a19660c278125a1f8c25462
brunolourencosilva <brunolourenco.silva@gmail.com>
2026-03-10 19:11:36 -0300

    Limpa nesse lixo

--- testes_aleatorios/Gestão_escolar/src/modules/filtro.js
+++
@@ -1,8 +0,0 @@
-export function filtrarSituacao(estruturaDados) {
-    const situacao = '';
-
-    estruturaDados.forEach(estruturaDados => {
-        situacao = (estruturaDados.frequencia >= 75 && estruturaDados.media >= 60) ? 'Aprovado' : 'Reprovado'
-    })
-    return situacao;
-}
--- testes_aleatorios/Gestão_escolar/src/modules/situacao.js
+++
@@ -1,8 +0,0 @@
-export function verificarSituacao (media, frequencia) {
-    if (frequencia >=75 && media >= 15) {
-        return 'aprovado';
-    } else {
-        return 'Reprovado';
-
-    }
-}
