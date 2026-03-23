ae980cf6de9f43d011a8f3db6eb37230c096d299
brunolourencosilva <brunolourenco.silva15@gmail.com>
2026-03-23 13:39:06 -0300

    FEAT:Mudança de tamahnhos no css

--- criar_personagem_hab.php
+++ criar_personagem_hab.php
@@ -29,103 +29,106 @@
         <div class="titulo">
             <h1>Criação do Personagem</h1>
         </div>
-        
-        <form action="criar_personagem_skill.php" method="GET" id="formPersonagem">
-            <!-- ============== MENU CARACTERIZAÇÕES ============== -->
-            <div class="menu-info-character">
+        <div class="organizador-hab">
+            <form action="criar_personagem_skill.php" method="GET" id="formPersonagem">
+                <!-- ============== MENU CARACTERIZAÇÕES ============== -->
+                <div class="menu-info-character">
 
-                <!-- NOME -->
-                <div class="box-nome">
-                    <div class="terminal-box borda-neon">
-                        <span class="cursor"></span>
+                    <!-- NOME -->
+                    <div class="box-nome">
+                        <div class="terminal-box borda-neon">
+                            <span class="cursor"></span>
 
-                        <input 
-                        class="terminal-input"
-                        type="text"
-                        name="personagem"
-                        placeholder="Nome do personagem"
-                        maxlength="20"
-                        required>
+                            <input 
+                            class="terminal-input"
+                            type="text"
+                            name="personagem"
+                            placeholder="Nome do personagem"
+                            maxlength="20"
+                            required>
+                        </div>
+                    </div>
+
+                    <!-- ATRIBUTOS -->
+                    <div class="box-caracteristicas borda-neon">
+
+                        <h2>Caracterização</h2>
+
+                        <p id="pontosRestantes">21 Pontos restantes</p>
+
+                        <!-- FORÇA -->
+                        <div class="linha-caract">
+                            <button type="button" class="btn btn-pts">‹</button>
+                            <span class="ptsCaracterizacao">00</span>
+                            <button type="button" class="btn btn-pts">›</button>
+                            <span class="nome-caract">Força</span>
+                        </div>
+
+                        <!-- INT -->
+                        <div class="linha-caract">
+                            <button type="button" class="btn btn-pts">‹</button>
+                            <span class="ptsCaracterizacao">00</span>
+                            <button type="button" class="btn btn-pts">›</button>
+                            <span class="nome-caract">Inteligência</span>
+                        </div>
+
+                        <!-- PERCEPÇÃO -->
+                        <div class="linha-caract">
+                            <button type="button" class="btn btn-pts">‹</button>
+                            <span class="ptsCaracterizacao">00</span>
+                            <button type="button" class="btn btn-pts">›</button>
+                            <span class="nome-caract">Percepção</span>
+                        </div>
+
+                        <!-- AGILIDADE -->
+                        <div class="linha-caract">
+                            <button type="button" class="btn btn-pts">‹</button>
+                            <span class="ptsCaracterizacao">00</span>
+                            <button type="button" class="btn btn-pts">›</button>
+                            <span class="nome-caract">Agilidade</span>
+                        </div>
+
+                        <!-- CARISMA -->
+                        <div class="linha-caract">
+                            <button type="button" class="btn btn-pts">‹</button>
+                            <span class="ptsCaracterizacao">00</span>
+                            <button type="button" class="btn btn-pts">›</button>
+                            <span class="nome-caract">Carisma</span>
+                        </div>
+
+                        <!-- DURABILIDADE -->
+                        <div class="linha-caract">
+                            <button type="button" class="btn btn-pts">‹</button>
+                            <span class="ptsCaracterizacao">00</span>
+                            <button type="button" class="btn btn-pts">›</button>
+                            <span class="nome-caract">Durabilidade</span>
+                        </div>
+
+                        <!-- SORTE -->
+                        <div class="linha-caract">
+                            <button type="button" class="btn btn-pts">‹</button>
+                            <span class="ptsCaracterizacao">00</span>
+                            <button type="button" class="btn btn-pts">›</button>
+                            <span class="nome-caract">Sorte</span>
+                        </div>
+
                     </div>
                 </div>
 
-                <!-- ATRIBUTOS -->
-                <div class="box-caracteristicas borda-neon">
-
-                    <h2>Caracterização</h2>
-
-                    <p id="pontosRestantes">21 Pontos restantes</p>
-
-                    <!-- FORÇA -->
-                    <div class="linha-caract">
-                        <button type="button" class="btn btn-pts">‹</button>
-                        <span class="ptsCaracterizacao">00</span>
-                        <button type="button" class="btn btn-pts">›</button>
-                        <span class="nome-caract">Força</span>
-                    </div>
-
-                    <!-- INT -->
-                    <div class="linha-caract">
-                        <button type="button" class="btn btn-pts">‹</button>
-                        <span class="ptsCaracterizacao">00</span>
-                        <button type="button" class="btn btn-pts">›</button>
-                        <span class="nome-caract">Inteligência</span>
-                    </div>
-
-                    <!-- PERCEPÇÃO -->
-                    <div class="linha-caract">
-                        <button type="button" class="btn btn-pts">‹</button>
-                        <span class="ptsCaracterizacao">00</span>
-                        <button type="button" class="btn btn-pts">›</button>
-                        <span class="nome-caract">Percepção</span>
-                    </div>
-
-                    <!-- AGILIDADE -->
-                    <div class="linha-caract">
-                        <button type="button" class="btn btn-pts">‹</button>
-                        <span class="ptsCaracterizacao">00</span>
-                        <button type="button" class="btn btn-pts">›</button>
-                        <span class="nome-caract">Agilidade</span>
-                    </div>
-
-                    <!-- CARISMA -->
-                    <div class="linha-caract">
-                        <button type="button" class="btn btn-pts">‹</button>
-                        <span class="ptsCaracterizacao">00</span>
-                        <button type="button" class="btn btn-pts">›</button>
-                        <span class="nome-caract">Carisma</span>
-                    </div>
-
-                    <!-- DURABILIDADE -->
-                    <div class="linha-caract">
-                        <button type="button" class="btn btn-pts">‹</button>
-                        <span class="ptsCaracterizacao">00</span>
-                        <button type="button" class="btn btn-pts">›</button>
-                        <span class="nome-caract">Durabilidade</span>
-                    </div>
-
-                    <!-- SORTE -->
-                    <div class="linha-caract">
-                        <button type="button" class="btn btn-pts">‹</button>
-                        <span class="ptsCaracterizacao">00</span>
-                        <button type="button" class="btn btn-pts">›</button>
-                        <span class="nome-caract">Sorte</span>
-                    </div>
-
-                </div>
-            </div>
+                
+            </form>
 
             <div class="ocultar">
                 <div class="borda-neon icone-habilidade"></div>
                 <div class="descricao-hab">
                     <p >
                     Quanto maior a Força Bruta, maior o dano em combate físico e melhor
-                    o uso de armas pesadas ou ações que exigem força. Skills afetadas:
+                    o uso de armas pesadas ou ações que exigem força.<br><br>
+                    Skills afetadas:<br>
                     Corpo a Corpo, Armas Pesadas, capacidade de carga.<p>
                 <div>
             </div>
-
-        </form>
+        </div>
 
         <script src="public/js/main-menu/script.js"></script>
 
