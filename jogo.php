07219eab48a9f523d3b4f7180442c59c7a190d0e
brunolourencosilva <brunolourenco.silva15@gmail.com>
2026-03-25 15:24:44 -0300

    FIX: Tamanho da caixa de dialogo e imagens arrumados

--- jogo.php
+++ jogo.php
@@ -26,7 +26,16 @@
                 <div class="organizador-cutscene"> 
                     <div class="img-cutscene borda-neon" id="cutscene-inicio"></div>
                 </div>
-                <div>
+                
+                <div class="container-dialogo borda-neon">
+                    <p class="dialogo">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <p>
+                    <button>Continuar -> </button>
                 </div>
 
             </main>
